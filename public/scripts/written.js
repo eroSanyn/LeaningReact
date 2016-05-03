eccd1629271026bc2410ec47d1da9fad292c6e1c
eroSanyn <sasideepbommareddy@gmail.com>
2016-05-03 17:19:28 -0400

    followed the tutorial for commentbox

--- public/scripts/written.js
+++ public/scripts/written.js
@@ -1,21 +1,33 @@
 var CommentBox = React.createClass({
 	displayName: 'CommentBox',
-	render: function(){
-		return (<h1>Hello World!</h1>);
-	}
+	render: function() {
+    	return (
+     		<div className="commentBox">
+	        	<h1>Comments</h1>
+		        <CommentList />
+		        <CommentForm />
+	      	</div>
+    );
+  }
 });
 
 var CommentList = React.createClass({
 	render: function(){
 		return (
 			<div>
-			  <CommentBox />
 			<div>Maybe here is!</div>
-			</div>);
+			</div>
+			);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return (<div>form here</div>);
 	}
 });
 
 ReactDOM.render(
-	React.createElement(CommentList, null), 
+	React.createElement(CommentBox, null), 
 	document.getElementById('content')
 	);
