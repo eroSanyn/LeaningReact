f84f75f454b5aec9708604fc469edfac79607628
sasideep <sasideep.bommareddy@perficient.com>
2016-05-03 16:29:20 -0400

    updated written.js to include comments header and stuff along with file to track what I learn

--- public/scripts/written.js
+++ public/scripts/written.js
@@ -1,8 +1,21 @@
-var commentBox = React.createClass({
+var CommentBox = React.createClass({
+	displayName: 'CommentBox',
 	render: function(){
 		return (<h1>Hello World!</h1>);
 	}
 });
 
+var CommentList = React.createClass({
+	render: function(){
+		return (
+			<div>
+			  <CommentBox />
+			<div>Maybe here is!</div>
+			</div>);
+	}
+});
+
 ReactDOM.render(
-	React.createElement(commentBox),  document.getElementById('content'));
+	React.createElement(CommentList, null), 
+	document.getElementById('content')
+	);
